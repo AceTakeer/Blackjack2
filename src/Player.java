221dbd8485b751a396810a426012327fa3ce0ebf
tLovesee <tloveseework@gmail.com>
2024-10-03 15:50:54 -0500

    player class

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,62 @@
+import java.util.Random;
+
+
+
 public class Player{
-  
+    Random rand = new Random();
+
+//Variables
+    int deckSize;
+    int[] deck = new int[50];
+    int score;
+    boolean isBust;
+
+    public Player(){
+
+        deckSize = 2;
+        score = 0;
+
+    }
+
+//Gets
+    public int[] getDeck(){
+        return this.deck;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public boolean getBust(){
+        return this.isBust;
+    }
+//Sets
+    public void setScore(int ns){
+        this.score+=ns;
+    }
+    public void setBust(boolean nb){
+        this.isBust = nb;
+    }
+
+    public void addCard(){
+        
+        deck[deckSize] = rand.nextInt(11) + 2;
+        deckSize++;
+
+    }
+
+    public void reset(){
+        for (int i: deck){
+            deck[i] = 0;
+        }
+        deckSize = 2;
+        isBust = false;
+        score = 0;
+    }
+
+
+
+
+
+
 }
